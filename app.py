5b7aa8155097aa89af26589a8979ecf078ad8262
tejas dixit <tejasdixit17@gmail.com>
2020-12-31 02:52:42 +0530

    updated code

--- app.py
+++ app.py
@@ -1,74 +1,69 @@
 import webbrowser as wb
-import wikipedia as wk
-import os
+import wikipedia as wk 
+from os import system, name 
+from time import sleep 
 import smtplib as s 
 from login import Login 
 import getpass as gp 
+import errors as e
+import pyfiglet 
+  
 
-assistant_name = "PANDA"
+assistant_name = "[PANDA]"
 
 def show_choice():
     clear()
-    show_ch = """
-    --------------- SERVICES BY PANDA-CLI-Assistant --------------
-    NOTE :- 
-        
-        1) Enter the service no you want to execte.
-
+    result = pyfiglet.figlet_format("PANDA-CLI-Assistant",font="slant") 
+    show_ch =f"""
+    -------------------- SERVICES BY PANDA-CLI-Assistant --------------
+    
+    {result}
+    -By Mr.Tejas Dixit 
+    -------------------------------------------------------------------
+    NOTE :-  
+        1) Enter the service number  you want to execute.
+    -------------------------------------------------------------------
     SERVICES :- 
-
+    -------------------------------------------------------------------
         1)Browsing 
         2)Wikipedia 
         3)You-Tube 
         4)Email
         5)Devloper Guide 
         6)Exit from 
-
     -------------------------------------------------------------------
 
               """
+             
     print(show_ch)
         
-
-#   END SHOW FUNCATION 
-
-
-#-------------------------------------------------------------------------------------------------------------------------
-
-# START OF GET CHOICE FUNCATION  
-
 def get_choice():
-    
     show_choice()
     try:
-        choice = int(input(f"[{assistant_name}]Enter Your choice : \n\n"))
-    except: 
-       
+        choice = input(f"{assistant_name}Enter Your choice :\n")
+    except:
         return get_choice()
-    if choice == 1:
+
+    if choice == '1':
+        return Browsing()
+    elif choice == '2':
         return wiki()
-    elif choice == 2:
-        return sendmail()
-    elif choice == 3:
-        return search()
-    elif choice == 4:
-        return dinfo()
-    elif choice == 5:
+    elif choice == '3':
         return youtube()
-
-# END OF GET CHOICE FUNCATION 
-
-#-------------------------------------------------------------------------------------------------------------------------
-
-# START WIKIPEDIA FUUNCATION
+    elif choice == '4':
+        return sendmail()
+    elif choice == '5':
+        return dinfo()
+    else:
+       return e.incorrectChoiceError()
 
 def wiki():
     clear()
     try:
-        topic = input(" [pandatd] Enter Your Topic-Name   To Search :\n\n [You]").lower()
-        print(' [pandatd] You Enterd this topic : ',topic)
+        topic = input(f"{assistant_name} Enter Your Topic-Name   To Search :\n\n [You]").lower()
+        print(f'{assistant_name} You Enterd this topic : ',topic)
         print('\n')
-        print("[pandatd] Releted searches are : \n")
+        print(f"{assistant_name} Releted searches are : \n")
         result = wk.search(topic)
 
         i = 1
@@ -79,50 +74,44 @@ def wiki():
         print('\n')
 
         print('\t',dec,' ')
-        get_ch = input('[pandatd] Do you want  to save teh output [y | n]\n[you]').lower()
+        get_ch = input(f'{assistant_name} Do you want  to save teh output [y | n]\n[you]').lower()
         if get_ch == 'y' :
-            getfnm = input("[pandatd]Type name to save the file : \n[you]")
+            getfnm = input(f"{assistant_name}Type name to save the file : \n[you]")
             f = open (getfnm,'w')
             dec = wk.summary(topic)
             f.write(dec)
             f.close()    
     except:
-        print('[PANDATD] PLEASE MUST WRITE QUERY OR, \n \
+        print(f'{assistant_name} PLEASE MUST WRITE QUERY OR, \n \
         CHECK SPELLING MESTAKES,\n\t PLEASE TRY AGAIN LATER,\
         \n\t AND CHECK INTERNET CONNECATION OR TURN OFF YOUR AEROPLANE MODE ')
         print('\n')
-        get = input('[pandatd] Do you want search again if yes press y or press any key :\n[you]')
+        get = input(f'{assistant_name} Do you want search again if yes press y or press any key :\n[you]')
         if get == 'y':
             return wiki()
     return get_choice ()
 
-# END OF WIKIPEDIA FUNCATION
-
-#-------------------------------------------------------------------------------------------------------------------------
-
-# START OF SENDNAIL FUNCATION  
-
 def sendmail():
     clear()
-    print('Wait for some time ...! ')
+    print(f'{assistant_name}Wait for some time ...! ')
     try:
         ob = s.SMTP('smtp.gmail.com',587) 
-        mailadd = input('[pandatd]Enter your email\n\n[you]').lower()
-        password = gp.getpass('[pandatd] Enter your password \n\n[you]')
+        mailadd = input(f'{assistant_name}Enter your email\n\n[you]').lower()
+        password = gp.getpass(f'{assistant_name} Enter your password \n\n[you]')
        
         try:
             ob.starttls()
-            print('Esatbish secure connecation ...') 
-            print('Connecation Established !')
+            print(f'{assistant_name}Esatbish secure connecation ...') 
+            print(f'{assistant_name}Connecation Established !')
             ob.login(mailadd,password)
-            print('Trying to login in...')
-            print('Login Sucessfully !')
+            print(f'{assistant_name}Trying to login in...')
+            print(f'{assistant_name}Login Sucessfully !')
           
-            resiver = input('[pandatd] Enter resiver email.\n\n[you]')
+            resiver = input(f'{assistant_name} Enter resiver email.\n\n[you]')
             
-            subject = input('[pandatd] Enter Subject for email.\n\n[you]')
+            subject = input(f'{assistant_name} Enter Subject for email.\n\n[you]')
             
-            body = input('[pandatd] Enter Content for email.\n\n[you]')
+            body = input(f'{assistant_name} Enter Content for email.\n\n[you]')
             
             message = "subject :{}\n\nBody:{}".format(subject,body) 
             print(message)
@@ -133,65 +122,55 @@ def sendmail():
             print('Email send sucessfully !! ')
             print('Connecation Closed !!')
         except:
-            print('[pandatd] ENTER VALID EMAIL AND PASSWORD ')
+            print(f'{assistant_name} ENTER VALID EMAIL AND PASSWORD ')
     except:
-        print('[PANDATD] PLEASE TRY AGAIN LATER,\
+        print(f'{assistant_name} PLEASE TRY AGAIN LATER,\
         \n\t AND CHECK INTERNET CONNECATION OR TURN OFF YOUR AEROPLANE MODE') 
         print('')
 
     return get_choice()
 
-# END MAIL FUNCATION
-
-#-------------------------------------------------------------------------------------------------------------------------
-
-# START SEARCH FUNCATION
-
-def search():
-    clear()
+def Browsing():
+    choice_list =f"""
+    -------------------------------------------------------------------
+    NOTE:- Select your search engine :)
+    -------------------------------------------------------------------
+        1.1)duckduckgo [ Deafult and privecy based ]
+        1.2)google [ Recommonded when you want exact result and fast ]
+    -------------------------------------------------------------------    
+            """
+    print(choice_list)
     try:
-        search1 = input('[pandatd] Enter Text What you Have to Search !\n\n[you]')
-        print('\n')
-        print('Tab no 1 :', search1)
-        t.sleep(0.1)
-        wb.open_new('https://www.google.com/search?q='+search1)
-
-        
+        choice = input(f"{assistant_name}Enter Your choice :\n")
+        if choice == '1.1':
+            search1 = input(f'{assistant_name} Enter Text What you Have to Search !\n[you]')
+            print('Tab no 1 :', search1)
+            wb.open_new_tab('https://www.duckduckgo.com/?q='+search1)
+        elif choice == '1.2':
+            search1 = input(f'{assistant_name} Enter Text What you Have to Search !\n[you]')
+            print('Tab no 1 :', search1)
+            wb.open_new_tab('https://www.google.com/search?q='+search1)
+        else :
+            return e.incorrectChoiceError()        
     except:
-        print('[pandatd] PLEASE CHECK YOUR URL AGAIN,\n \
-            CHECK YOUR INTERNET CONNECTION,\n \
-            AND TURN OFF YOUR AEROPLANE MODE ')
+        return e.offlineError()
         
-        print('\n')
-
-        get = input('[pandatd] Do you want search again if yes press y or press any key ')
-        if get == 'y':
-            return search()       
-    return get_choice()
-
-# END OF SEARCH FUNCATION  
-
-#-------------------------------------------------------------------------------------------------------------------------
-
-# START YOUTUBE FUNCATION 
+    get = input(f'{assistant_name} Do you want search again[Y/n]').lower()
+    if get == 'y':
+        return Browsing() 
+    else:
+        return get_choice()
 
 def youtube():
     clear()
     try:
-        searchq = input('[pandatd]Enter youtube serach : ')
+        searchq = input(f'{assistant_name}Enter youtube serach : ')
         t.sleep(1)
-        print('[pandatd] Opening Youtube within your search! \n')
+        print(f'{assistant_name} Opening Youtube within your search! \n')
         wb.open_new('www.youtube.com/results?search_query='+searchq) 
     except:
-        print("[pandatd] Unknow Error or try after some time ok ! ")
+        print(f"{assistant_name} Unknow Error or try after some time ok ! ")
     return get_choice()
-
-# END OF YOUTUBE FUNCATION 
-
-#-------------------------------------------------------------------------------------------------------------------------
-   
-# DEVLOPER FINCATION 
-
 
 def dinfo():
     clear()
@@ -205,24 +184,12 @@ def dinfo():
     \t\tFolllow us on \n\
     \nInstagram:',Instagram,'\nGitHub:',GitHub,'\nTwitter:',Twitter,'\nEmail',Email)
     return get_choice()
-
-    
-# END OF DEVLOPER FUNCATION
-
-#-------------------------------------------------------------------------------------------------------------------------
-# import only system from os 
-from os import system, name 
-
-# import sleep to show output for some time period 
-from time import sleep 
-
-# define our clear function 
+ 
 def clear(): 
-
-	# for windows 
 	if name == 'nt': 
+        t.sleep(1)
 		_ = system('cls') 
-
 	# for mac and linux(here, os.name is 'posix') 
 	else: 
+        t.sleep(1)
 		_ = system('clear') 
